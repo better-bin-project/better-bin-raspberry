ac335ed8429758748a88c79dfa61db8e24dd1f5c
Finn Rudolph <finn.rudolph@t-online.de>
2022-06-21 21:49:58 +0200

    Simplify and refactor

--- betterbin.py
+++ betterbin.py
@@ -13,8 +13,6 @@ STEPS_BOX = 100 # small motor
 
 class Sftp:
     def __init__(self, hostname, username, password, port=22):
-        """Constructor Method"""
-        # Set connection object to None (initial value)
         self.connection = None
         self.hostname = hostname
         self.username = username
@@ -22,12 +20,10 @@ class Sftp:
         self.port = port
 
     def connect(self):
-        """Connects to the sftp server and returns the sftp connection object"""
         cnopts = pysftp.CnOpts()
         cnopts.hostkeys = None
 
         try:
-            # Get the sftp connection object
             self.connection = pysftp.Connection(
                 host=self.hostname,
                 username=self.username,
@@ -35,53 +31,34 @@ class Sftp:
                 port=self.port,
                 cnopts=cnopts,
             )
+
         except Exception as err:
             raise Exception(err)
+
         finally:
-            print(f"Connected to {self.hostname} as {self.username}.")
+            print(f"connected to {self.hostname} as user {self.username}")
 
     def disconnect(self):
-        """Closes the sftp connection"""
         self.connection.close()
-        print(f"Disconnected from host {self.hostname}")
+        print(f"disconnected from {self.hostname}")
 
     def upload(self, source_local_path, remote_path):
-        """
-        Uploads the source files from local to the sftp server.
-        """
-
         try:
-            print(
-                f"uploading to {self.hostname} as {self.username} [(remote path: {remote_path});(source local path: {source_local_path})]"
-            )
-
-            # Download file from SFTP
             self.connection.put(source_local_path, remote_path)
-            print("upload completed")
+            print(f"uploaded {source_local_path} to {self.hostname}")
 
         except Exception as err:
             raise Exception(err)
 
     def download(self, remote_path, target_local_path):
-        """
-        Downloads the file from remote sftp server to local.
-        Also, by default extracts the file to the specified target_local_path
-        """
-
         try:
-            print(
-                f"downloading from {self.hostname} as {self.username} [(remote path : {remote_path});(local path: {target_local_path})]"
-            )
-
-            # Download from remote sftp server to local
             self.connection.get(remote_path, target_local_path)
-            print("download completed")
+            print(f"downloaded {remote_path} from {self.hostname}")
 
         except Exception as err:
             raise Exception(err)
 
     def listdir(self, remote_path):
-        """lists all the files and directories in the specified path and returns them"""
         return self.connection.listdir(remote_path)
 
 sftp = Sftp('192.168.0.147', 'finn', os.environ.get('ENVY_PWD'))
--- stepper.py
+++ stepper.py
@@ -10,25 +10,26 @@ ENA_2 = 17
 DIR_2 = 27
 PUL_2 = 22
 
-forward1 = True
-forward2 = True
-
 for pin in [ENA_1, DIR_1, PUL_1, ENA_2, DIR_2, PUL_2]:
     GPIO.setup(pin, GPIO.OUT)
 
 GPIO.output(ENA_1, GPIO.LOW)
 GPIO.output(ENA_2, GPIO.LOW)
-GPIO.output(DIR_1, GPIO.HIGH if forward1 else GPIO.LOW)
-GPIO.output(DIR_2, GPIO.HIGH if forward2 else GPIO.LOW)
+GPIO.output(DIR_1, GPIO.HIGH)
+GPIO.output(DIR_2, GPIO.HIGH)
 
 # Does n steps with interval t (in s)
 # motorNum: 1 -> big motor; 2 -> small motor
+# n < 0 --> backwards movement
 def doSteps(motorNum, n, t):
-    numSteps = n if n > 0 else -n
-    if n < 0:
-        reverse(motorNum)
-    pul_pin = PUL_1 if motorNum == 1 else PUL_2
     ena_pin = ENA_1 if motorNum == 1 else ENA_2
+    dir_pin = DIR_1 if motorNum == 1 else DIR_2
+    pul_pin = PUL_1 if motorNum == 1 else PUL_2
+    numSteps = n if n > 0 else -n
+
+    if n < 0:
+        GPIO.output(dir_pin, GPIO.LOW)
+        sleep(0.5)
     GPIO.output(ena_pin, GPIO.HIGH)
 
     for i in range(numSteps):
@@ -36,27 +37,14 @@ def doSteps(motorNum, n, t):
         sleep(t / 2)
         GPIO.output(pul_pin, GPIO.LOW)
         sleep(t / 2)
+
     GPIO.output(ena_pin, GPIO.LOW)
     if n < 0:
-        reverse(motorNum)
-
-def reverse(motorNum):
-    global forward1
-    global forward2
-    dir_pin = DIR_1 if motorNum == 1 else DIR_2
-    forward = forward1 if motorNum == 1 else forward2
-    GPIO.output(dir_pin, GPIO.LOW if forward else GPIO.HIGH)
-    if motorNum == 1:
-        forward1 = not forward1
-    else:
-        forward2 = not forward2
-    sleep(1)
+        GPIO.output(dir_pin, GPIO.HIGH)
+        sleep(0.5)
 
 if __name__ == '__main__':
     while True:
         print("Enter [motor number] [number of steps] [interval]: ")
         arr = input().split(' ')
-        motorNum = int(arr[0])
-        numSteps = int(arr[1])
-        interval = float(arr[2])
-        doSteps(motorNum, numSteps, interval)
+        doSteps(int(arr[0]), int(arr[1]), float(arr[2]))
